1b50392629c9dab2fb664b099ecd6da3e1613cfe
Cathy Diaz <Cathydiaz14@gmail.com>
2021-10-01 12:48:25 -0700

    commit tree 5fbd790a0c2163ef63184f7c1385d88d253fef03

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,31 @@
 import React from "react";
 
+
+//this component takes one prop: maxChars which is a number(maximum number of characters a message can have)
+//this prop is being passed in from the App component with value 280
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
+
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message} />
+        {charNumber}
       </div>
     );
   }
